9ef00b15d24379e2c19e4cdcdf86728cb81955f6
KianLee <lijian112172@qq.com>
2015-10-25 23:26:31 +0800

    页面提交

--- js/common.js
+++ js/common.js
@@ -1,3 +1,22 @@
 $(function(){
+	
+	$('.btnVote').click(function(){
+		console.log($(this).val());
+		
+		$('.btnVote').each(function(index, element) {
+			$(this).parents('.tit').hide();
+			var $res = $(this).parents('.tit').siblings('.result');
+				result = $res.find('em').text();
+			
+			$res.show().find('span').animate({width: result}, 500);	
+			
+		});
+		
+	});
+	
+	$('#repForm').submit(function(){
+		$('.back',this).show();
+		return false;
+	});
 
 });
