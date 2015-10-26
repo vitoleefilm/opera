79fbe09593324f7b919b1b2fa8c7d477255e62a2
KianLee <lijian112172@qq.com>
2015-10-26 21:07:34 +0800

    1. 兼容首页一屏展示 2. 群众观点-视屏插入

--- js/common.js
+++ js/common.js
@@ -1,15 +1,22 @@
 $(function(){
 	
+	if($('.hpMenu').size() > 0){
+		$('.hpMenu').each(function(index, element) {
+			var h = ($(window).height() - $(this).position().top) / 5 - 1;
+			$('li a', this).each(function(index, element) {
+				$(this).height(h)
+				$(this).css('line-height', h + 'px');
+			});
+		});
+	};
+	
 	$('.btnVote').click(function(){
-		console.log($(this).val());
 		
 		$('.btnVote').each(function(index, element) {
 			$(this).parents('.tit').hide();
 			var $res = $(this).parents('.tit').siblings('.result');
 				result = $res.find('em').text();
-			
 			$res.show().find('span').animate({width: result}, 500);	
-			
 		});
 		
 	});
