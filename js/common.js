c6df555d49f3fcc4414a249cc3a670b6b4a30bfb
lijian112172 <lijian112172@qq.com>
2016-03-16 17:05:56 +0800

    调整

--- js/common.js
+++ js/common.js
@@ -1,4 +1,32 @@
 $(function(){
+	/* Mobile */
+	isMobile = {  
+		Android: function() {  
+			return navigator.userAgent.match(/Android/i) ? true : false;  
+		},  
+		BlackBerry: function() {  
+			return navigator.userAgent.match(/BlackBerry/i) ? true : false;  
+		},  
+		iOS: function() {  
+			return navigator.userAgent.match(/iPhone|iPad|iPod/i) ? true : false;  
+		},  
+		Windows: function() {  
+			return navigator.userAgent.match(/IEMobile/i) ? true : false;  
+		},  
+		any: function() {  
+			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Windows());  
+		},
+		iPad :function(){
+			return navigator.userAgent.match(/iPad/i)? true : false;
+		},
+		Status: function() {  
+			return checkLayout();  
+		}
+	};
+	
+	if(!isMobile.any()){
+		$('#pcshow').show();
+	};
 	
 	$('.btnMenu').click(function(e){
 		e.preventDefault();
